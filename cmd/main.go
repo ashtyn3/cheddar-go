161f9863073205b0be9ce80beb8cdb1a076fe3e2
ashtyn <ashtynmorel@gmail.com>
2024-06-13 16:38:54 -0400

    chore: fix leak

--- cmd/main.go
+++ cmd/main.go
@@ -8,11 +8,11 @@ import (
 func main() {
 	inc := new(cheddar.Instance).New("db")
 
-	t := new(cheddar.Table).New(inc.Pool, "jenna", 1)
-	c := new(cheddar.Column).New(inc.Pool, "jeter", cheddar.INT64)
+	t := new(cheddar.Table).New(inc.Pool, "a", 1)
+	c := new(cheddar.Column).New(inc.Pool, "a", cheddar.INT64)
 	t.Column(c)
 
 	// inc.InsertTable(t)
-	fmt.Println(inc.GetColumn("jenna", "jeter"))
+	fmt.Println(inc.GetColumn("a", "a"))
 	inc.Trace()
 }
